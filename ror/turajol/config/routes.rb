cf1624af83bb193b0ea86773474b336d07398e56
igor <write.to.lee.igor@gmail.com>
2016-12-26 00:07:06 +0600

    Revert "#13 Сделал линк пути /points на points#index"
    This reverts commit ea482c9389c6e41566147338e0427d661e4412d7.

--- ror/turajol/config/routes.rb
+++ ror/turajol/config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'points/', to: 'points#index', as: 'points'
 end
