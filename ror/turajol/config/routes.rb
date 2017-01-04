556b7ff1149ba8bbc52abf1fea41521dde87c367
igor <write.to.lee.igor@gmail.com>
2017-01-04 22:07:31 +0600

    #15 Сделал линк пути /points на points#index

--- ror/turajol/config/routes.rb
+++ ror/turajol/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/points', to: 'points#index', as: 'points'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
