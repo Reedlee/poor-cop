ae3e2def39a7da65413db54ea946ccacf403999e
igor <write.to.lee.igor@gmail.com>
2017-01-06 23:51:05 +0600

    #14 Добавил тесты на валидацию

--- ror/turajol/test/models/point_test.rb
+++ ror/turajol/test/models/point_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class PointTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "latitude should be presented " do
+    point = Point.new
+    point.longitude = '74.612757'
+
+    assert_not point.save
+  end
+
+  test "longitude should be presented " do
+    point = Point.new
+    point.latitude = '42.87793'
+
+    assert_not point.save
+  end
+
+  test "coordinates should have two digest before point" do
+    point = Point.new
+    point.latitude = '42.87793'
+    point.longitude = '743.612757'
+
+    assert_not point.save
+  end
+
+  test "courdinates should consist from integer" do
+    point = Point.new
+    point.latitude = '42.87k793'
+    point.longitude = '74.612757'
+
+    assert_not point.save
+  end
+
+  test "courdinates should have the `.`" do
+    point = Point.new
+    point.latitude = '4287793'
+    point.longitude = '74.612757'
+
+    assert_not point.save
+  end
+
 end
