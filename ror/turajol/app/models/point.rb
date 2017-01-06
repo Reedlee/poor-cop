be1acbafb10e79928c01bba0eba091c288294a8e
igor <write.to.lee.igor@gmail.com>
2017-01-06 23:50:35 +0600

    #14 Создал простую проверку, чтобы долгота и  широта были заполненны, и чтобы они были формата xx.xxxxxx - где х является числом

--- ror/turajol/app/models/point.rb
+++ ror/turajol/app/models/point.rb
@@ -1,2 +1,10 @@
 class Point < ApplicationRecord
+  FORMAT_COORDINATES = /\A[\d]{2}[\.][\d]*\z/
+  MESSAGE_ERORR_FORMAT = 'Only digital and point accepted'
+
+  validates :latitude, presence: true
+  validates :longitude, presence: true
+
+  validates :latitude, format: {with: FORMAT_COORDINATES, message: MESSAGE_ERORR_FORMAT}
+  validates :longitude, format: {with: FORMAT_COORDINATES, message: MESSAGE_ERORR_FORMAT}
 end
