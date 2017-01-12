e1900de8e1ed791f5ccbaa67a2461617cd5e995f
Avaz Abdrasulov <avaz@Avazs-MacBook-Pro.local>
2017-01-13 00:50:00 +0600

    Недоделанный #13 тикет

--- ror/turajol/app/controllers/points_controller.rb
+++ ror/turajol/app/controllers/points_controller.rb
@@ -6,4 +6,24 @@ class PointsController < ApplicationController
       format.json {render json: @points.to_json(except: :id)}
     end
   end
+
+  def new
+    @point = Point.new
+  end
+
+  def create
+    @point = Point.create(point_parameters)
+  # binding.pry
+    if @point.errors.empty?
+      render text: params.inspect
+      redirect_to point_path(@point)
+    else
+       render text: "error"
+       render "new"
+    end
+  end
+
+  def point_parameters
+    params.require(:point).permit(:latitude, :longitude)
+  end
 end
--- ror/turajol/config/routes.rb
+++ ror/turajol/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get '/points', to: 'points#index', as: 'points'
+
+  resources :points, :only => [:index, :create, :new]
+  root 'points#index'
+
+  # get '/points', to: 'points#index', as: 'points'
+  # post '/point/new', to: 'points#create', as: 'new_point'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
