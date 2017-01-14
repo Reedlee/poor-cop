995ae0429b08c0455230bfff26a4159b82cca43f
igor <write.to.lee.igor@gmail.com>
2017-01-14 19:15:57 +0600

    #13 Создал временное решение: убрал проверку на наличия токена при отправки json объекта post. ЭТО очень не безопасное решение

--- ror/turajol/app/controllers/application_controller.rb
+++ ror/turajol/app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  #TODO переделать чтобы была валидация приложений, которые отправляет координаты
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
 end
