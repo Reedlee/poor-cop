7a9d2ec259f4c45bba53c39bb63abbf44d93de7e
igor <write.to.lee.igor@gmail.com>
2017-01-22 00:59:26 +0600

    #36 изменил текст сообщения при создание точки в неверном формате

--- ror/turajol/app/models/point.rb
+++ ror/turajol/app/models/point.rb
@@ -1,6 +1,6 @@
 class Point < ApplicationRecord
   FORMAT_COORDINATES = /\A[\d]{2}[\.][\d]*\z/
-  MESSAGE_ERORR_FORMAT = 'Only digital and point accepted'
+  MESSAGE_ERORR_FORMAT = 'Only digital, point accepted and mask should be xx.xxxxxxx'
 
   validates :latitude, presence: true
   validates :longitude, presence: true
