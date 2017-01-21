71449b04ad56fbdee5cac502489a319d7bdf65bf
igor <write.to.lee.igor@gmail.com>
2017-01-21 15:24:22 +0600

    #30 добавил настройки factory_girls для тестирования моделей

--- ror/turajol/spec/factories/points.rb
+++ ror/turajol/spec/factories/points.rb
@@ -1,5 +1,10 @@
 FactoryGirl.define do
-  factory :point do
-    
+  factory :point do |f|
+    f.latitude '42.87793'
+    f.longitude '74.612757'
+  end
+
+  factory :invalid_point, parent: :point do |f|
+    f.latitude nil
   end
 end
