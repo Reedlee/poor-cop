0a2d76a108655e5ffefb7f55029e926f0b4881a5
igor <write.to.lee.igor@gmail.com>
2017-01-21 15:45:34 +0600

    #36 создал метод, который удаляет точку

--- ror/turajol/app/controllers/points_controller.rb
+++ ror/turajol/app/controllers/points_controller.rb
@@ -21,10 +21,27 @@ class PointsController < ApplicationController
     else
       respond_to do |format|
         format.html{ render action: "new" }
-        format.json{ render json: @player.errors, status: :unprocessable_entity }
+        format.json{ render json: @point.errors, status: :unprocessable_entity }
       end
     end
   end
+
+  def destroy
+    @point = Point.find(params[:id])
+    if @point.destroy
+      respond_to do|format|
+        format.html{ redirect_to root_path}
+        format.json{render action: 'index', status: :deleted}
+      end
+    else
+      respond_to do|format|
+        format.html{redirect_to root_path}
+        format.json{render json: {message:'cannot delete'}, status: :unprocessable_entity}
+      end
+    end
+  end
+
+  private
 
   def point_parameters
     params.require(:point).permit(:latitude, :longitude)
