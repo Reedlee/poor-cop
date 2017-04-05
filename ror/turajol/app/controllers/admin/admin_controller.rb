d3c6c922d45d9ac930abd70973207fd73fe4fdcc
igor <write.to.lee.igor@gmail.com>
2017-04-05 21:08:28 +0600

    #31 Добавил базовую авторизацию для админа

--- ror/turajol/app/controllers/admin/admin_controller.rb
+++ ror/turajol/app/controllers/admin/admin_controller.rb
@@ -1,7 +1,11 @@
 module Admin
   class AdminController < ActionController::Base
+    NAME = ENV.fetch("LOGIN"){"admin"}
+    PASSWORD = ENV.fetch("PASSWORD"){"admin"}
     protect_from_forgery with: :exception
     add_flash_types :error, :success
+
+    http_basic_authenticate_with name: NAME, password: PASSWORD
   end
 end
 
